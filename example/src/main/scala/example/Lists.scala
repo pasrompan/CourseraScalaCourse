cccd511fd14923c833d60d22f4f49329db023690
Paschalis Rompanos <pas.rompan@gmail.com>
2022-08-20 18:01:52 +0300

    1st example assignment.

---
+++ example/src/main/scala/example/Lists.scala
@@ -0,0 +1,56 @@
+package example
+
+import scala.annotation.tailrec
+
+object Lists:
+
+  /**
+   * This method computes the sum of all elements in the list xs. There are
+   * multiple techniques that can be used for implementing this method, and
+   * you will learn during the class.
+   *
+   * For this example assignment you can use the following methods in class
+   * `List`:
+   *
+   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+   *    is empty an exception is thrown
+   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   *    list `xs` without its `head` element
+   *
+   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   *  solution.
+   *
+   * @param xs A list of natural numbers
+   * @return The sum of all elements in `xs`
+   */
+  def sum(xs: List[Int]): Int =
+    (xs: List[Int]) match
+      case Nil => 0 // empty list
+      case x :: Nil => x // list with only one element
+      case x :: tailL => x + sum(tailL)
+
+  /**
+   * This method returns the largest element in a list of integers. If the
+   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+   *
+   * You can use the same methods of the class `List` as mentioned above.
+   *
+   * ''Hint:'' Again, think of a recursive solution instead of using looping
+   * constructs. You might need to define an auxiliary method.
+   *
+   * @param xs A list of natural numbers
+   * @return The largest element in `xs`
+   * @throws java.util.NoSuchElementException if `xs` is an empty list
+   */
+  def max(xs: List[Int]): Int =
+  (xs: List[Int]) match
+    case Nil => throw new java.util.NoSuchElementException
+    case _ => maxHelp(0, xs)
+
+  @tailrec
+  def maxHelp(currentMax:Int, xs: List[Int]): Int =
+    (xs: List[Int]) match
+      case Nil => currentMax
+      case x :: Nil => if (x>currentMax) x else currentMax
+      case x :: tailL => if (x>currentMax) maxHelp(x, tailL) else maxHelp(currentMax, tailL)
